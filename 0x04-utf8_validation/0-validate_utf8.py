37cbc7f674435643e0f77799d9b8c357e49546df
ProgressAce <daiyaansallie7@gmail.com>
2024-02-01 06:31:42 +0200

    add simple solution and main files

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines function to validate if data is of utf-8 encoding."""
+
+
+def validUTF8(data):
+    """Validates if data is of utf-8 capable, at the 1 byte level only."""
+
+    if not isinstance(data, list):
+        return False
+
+    for int_char in data:
+        if int_char < 0 or int_char > 127:
+            return False
+
+    return True
