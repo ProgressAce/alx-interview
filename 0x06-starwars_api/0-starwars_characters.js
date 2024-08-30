3860aeba228e477429fc47ff72fa551a861f6f59
ProgressAce <daiyaansallie7@gmail.com>
2024-08-30 19:55:04 +0200

    Add possible task solution
    The program does not run as intended (an executable) on a windows machine.

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,46 @@
+#!/usr/bin/node
+// Displays all the characters retrieved from the used Star Wars API
+
+const request = require("request");
+const args = process.argv;
+
+if (args.length !== 3) {
+  console.log("USAGE: ./<programName> <movieID>");
+  process.exit(1);
+}
+
+const movieID = args[2];
+
+function requestUrl(url) {
+  return new Promise(function (resolve, reject) {
+    request(url, function (err, res, body) {
+      if (!err) {
+        const jsonResp = JSON.parse(body);
+        resolve(jsonResp);
+      } else {
+        reject(err);
+      }
+    });
+  });
+}
+
+request(
+  `https://swapi-api.alx-tools.com/api/films/${movieID}`,
+  async function printCharacters(err, res, body) {
+    if (!err) {
+      const jsonBody = JSON.parse(body);
+      const character_urls = jsonBody["characters"];
+
+      for (const url of character_urls) {
+        const person = await requestUrl(url);
+
+        if (person) {
+          console.log(person["name"]);
+        }
+      }
+
+    } else {
+      console.error("error: ", err);
+    }
+  }
+);
